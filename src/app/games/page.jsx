4c5ad12bc2b364deb01c26c06ad9c32f916a0fc0
Giovanni <giovanni.ggoncalves@outlook.com>
2025-05-15 08:41:30 -0300

    feat:create games page

---
+++ src/app/games/page.jsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+export default function Games() {
+    return (
+        <div className={styles.games}>
+            <h1>Página de Games</h1>
+        </div>
+    );
+}
